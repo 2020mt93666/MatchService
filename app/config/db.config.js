1062b65d3baad2a75d288ac2295da7cc6fba2199
2020mt93666 <2020mt93666@wilp.bits-pilani.ac.in>
2022-04-03 12:46:26 +0530

    Dockerize App

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,10 @@
+const {
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_PORT,
+  DB_NAME,
+} = process.env;
 module.exports = {
-  url: "mongodb+srv://sezg583:123@cluster0.5nnxd.mongodb.net"
+  url: `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`
 };
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
@@ -33,7 +34,7 @@ app.get("/", (req, res) => {
 require("./app/routes/match.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.NODE_DOCKER_PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
